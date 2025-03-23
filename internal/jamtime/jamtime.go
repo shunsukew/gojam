8998ca939afaf2352fba8c189e45faa193b70f02
Shunsuke Watanabe <shunsukesan.tech@gmail.com>
2025-03-23 21:22:45 +0900

    Added jam time basic impl

---
+++ internal/jamtime/jamtime.go
@@ -0,0 +1,49 @@
+package jamtime
+
+import "time"
+
+// JAM Common Era, the start of JAM protocol, 1200 UTC on January 1, 2025.
+var JAMCommonEra = time.Date(2025, time.January, 1, 12, 0, 0, 0, time.UTC)
+
+const (
+	TimeSlotsPerEpoch = 600                                  // E
+	TimeSlotDuration  = 6 * time.Second                      // P
+	EpochDuration     = TimeSlotsPerEpoch * TimeSlotDuration // E * P (= 3600)
+)
+
+type JAMTime struct {
+	seconds uint64
+}
+
+type Epoch uint32
+
+type TimeSlot uint32
+
+func Now() JAMTime {
+	now := time.Now().Unix()
+	return JAMTime{seconds: uint64(now - JAMCommonEra.Unix())}
+}
+
+func FromTime(t time.Time) (JAMTime, error) {
+	if t.Before(JAMCommonEra) {
+		return JAMTime{}, ErrTimeBeforeJAMCommonEra
+	}
+
+	return JAMTime{seconds: uint64(t.Unix() - JAMCommonEra.Unix())}, nil
+}
+
+func (jt *JAMTime) Seconds() uint64 {
+	return jt.seconds
+}
+
+func (jt *JAMTime) Epoch() Epoch {
+	return Epoch(jt.seconds / uint64(EpochDuration.Seconds()))
+}
+
+func (jt *JAMTime) TimeSlot() TimeSlot {
+	return TimeSlot(jt.seconds / uint64(TimeSlotDuration.Seconds()))
+}
+
+func (jt *JAMTime) Time() time.Time {
+	return JAMCommonEra.Add(time.Duration(jt.seconds) * time.Second)
+}
